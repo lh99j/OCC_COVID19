304c7cd75d41523c16f9f1afaa2cfa81ab3d8522
lh99j <grand2181@gmail.com>
2022-04-23 16:54:31 +0900

    AVD작동 오류로 인해 삭제, 초기 테스트

--- OCC_COVID19/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ OCC_COVID19/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,74 +3,28 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.SearchView
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.myapplication.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
-    private lateinit var toggle : ActionBarDrawerToggle
-
-    class MyFragmentPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity){
-        val fragments : List<Fragment>
-        init{
-            fragments = listOf(OneFragment(), TwoFragment(), ThreeFragment())
-        }
-
-        override fun getItemCount(): Int = fragments.size
-        override fun createFragment(position: Int): Fragment = fragments[position]
-    }
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-
-        //액션바의 내용을 툴바에 적용
-        setSupportActionBar(binding.toolbar)
-
-        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened, R.string.drawer_closed)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        toggle.syncState()
-
-
-        val adapter = MyFragmentPagerAdapter(this)
-        binding.viewpager.adapter = adapter
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu_main, menu)
-
-        val menuItem = menu.findItem(R.id.menu_search)
-        val searchView = menuItem.actionView as SearchView
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                Log.d("kkang", "search text : $query")
-                return true
-            }
-
-            override fun onQueryTextChange(p0: String?): Boolean {
-                return true
-            }
-        })
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        if(toggle.onOptionsItemSelected(item)){
-            return true
+        binding.btn1.setOnClickListener{
+            Log.d("lh99j", "btn1")
         }
 
-        return super.onOptionsItemSelected(item)
+        binding.btn2.setOnClickListener{
+            Log.d("lh99j", "btn2")
+        }
+
+        binding.btn3.setOnClickListener{
+            Log.d("lh99j", "btn3")
+        }
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
