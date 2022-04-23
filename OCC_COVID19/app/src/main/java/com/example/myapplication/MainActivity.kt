8877c3955d004b7217b6407f9e574c7b70d79ee4
lh99j <grand2181@gmail.com>
2022-04-23 16:48:17 +0900

    테스트

--- OCC_COVID19/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ OCC_COVID19/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,18 +3,74 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle : ActionBarDrawerToggle
 
-    private lateinit var binding:ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        binding.btn1.setOnClickListener {
-            Log.d("lh99j", "click!")
+    class MyFragmentPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity){
+        val fragments : List<Fragment>
+        init{
+            fragments = listOf(OneFragment(), TwoFragment(), ThreeFragment())
         }
+
+        override fun getItemCount(): Int = fragments.size
+        override fun createFragment(position: Int): Fragment = fragments[position]
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        //액션바의 내용을 툴바에 적용
+        setSupportActionBar(binding.toolbar)
+
+        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened, R.string.drawer_closed)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+
+
+        val adapter = MyFragmentPagerAdapter(this)
+        binding.viewpager.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+
+        val menuItem = menu.findItem(R.id.menu_search)
+        val searchView = menuItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.d("kkang", "search text : $query")
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return true
+            }
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
