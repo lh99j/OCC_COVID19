8c374d1e50af1078372a1e831d83fb8309dcd928
lh99j <grand2181@gmail.com>
2022-05-17 13:45:45 +0900

    openapi xml파일 연동

--- OCC_COVID19/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ OCC_COVID19/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.myapplication
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.RequiresApi
 import com.example.myapplication.databinding.ActivityMainBinding
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
+import javax.xml.parsers.DocumentBuilderFactory
 
 
 var text = ""
@@ -11,16 +18,17 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         binding.txt.text = ""
 
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+
         //키값
-        val key = "app key"
+        val key = "Decoding key"
         //현재 페이지번호
         val pageNo = "&pageNo=1"
         //한 페이지 결과 수
@@ -29,18 +37,65 @@ class MainActivity : AppCompatActivity() {
         val startCreateDt = "&startCreateDt=20220516"
         //검색할 생성일 범의의 끝
         val endCreateDt = "&endCreateDt=20220516"
-
+        //API 정보를 가지고 있는 주소
+        var url = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey="+key+pageNo+numOfRows+startCreateDt+endCreateDt
         // 버튼을 누르면 쓰레드 동작
-        binding.btn.setOnClickListener{
             //쓰레드 생성
+        binding.btn.setOnClickListener{
             val thread = Thread(NetworkThread(url))
             thread.start() //쓰레드 시작
-            thread.join() //멀티 작업 안되게 하려면 start 후 join 입력
-
+            thread.join()
             binding.txt.text = text
         }
+    }
+}
+
+class NetworkThread(
+var url: String): Runnable {
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun run() {
+
+        try {
+
+            val xml : Document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(url)
 
 
+            xml.documentElement.normalize()
 
+            //찾고자 하는 데이터가 어느 노드 아래에 있는지 확인
+            val list:NodeList = xml.getElementsByTagName("item")
+
+            //list.length-1 만큼 얻고자 하는 태그의 정보를 가져온다
+            for(i in 0..list.length-1){
+
+                val n:Node = list.item(i)
+
+                if(n.getNodeType() == Node.ELEMENT_NODE){
+
+                    val elem = n as Element
+
+                    val map = mutableMapOf<String,String>()
+
+
+                    // 이부분은 어디에 쓰이는지 잘 모르겠다.
+                    for(j in 0..elem.attributes.length - 1) {
+
+                        map.putIfAbsent(elem.attributes.item(j).nodeName, elem.attributes.item(j).nodeValue)
+
+                    }
+
+
+                    println("=========${i+1}=========")
+                    text += "${i + 1}번 캠핑장 \n"
+
+                    println("1. 주소 : ${elem.getElementsByTagName("createDt").item(0).textContent}")
+                    text += "1. 주소 : ${elem.getElementsByTagName("createDt").item(0).textContent} \n"
+
+                }
+            }
+        } catch (e: Exception) {
+            Log.d("TTT", "오픈API"+e.toString())
+        }
     }
 }
